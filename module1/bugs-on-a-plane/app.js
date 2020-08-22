93a8a62f21a040869b4d90bea76c0f9ad7c27bb6
Sabelyn Thorpe[B[D[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[A[B[B[B[B[B[C[C[D[D[D[D[C[C[C[Sabelyn Thorpe <sabelyn@outlook.com>
2020-08-22 12:35:15 -0400

    Completed Bugs on a Plane

---
+++ module1/bugs-on-a-plane/app.js
@@ -0,0 +1,28 @@
+var form = document.getElementById("airline-form");
+var submit = document.getElementById("submit");
+
+function formAlert(e) {
+	e.preventDefault();
+	
+    var firstName = form["first-name"].value;
+    var lastName = form["last-name"].value;
+    var age = form["age"].value;
+    var gender = form.gender.value;
+    var location = form["travel-location"].value;
+    var diet = [];
+    if (form.vegan.checked) {
+        diet.push(form.vegan.value);
+    }
+    if (form.gluten.checked) {
+        diet.push(form.gluten.value);
+    }
+    if (form.paleo.checked) {
+        diet.push(form.paleo.value);
+    }
+
+
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident...");
+}
+
+
+submit.addEventListener("click", formAlert);
